6bc7e2133fe159ba7ccc2ae6315d90bc289e0cbc
thibd <thib.del@free.fr>
2025-12-14 01:59:52 +0100

    ui: change dashboard mobile layout to 2 columns

--- app/page.tsx
+++ app/page.tsx
@@ -90,7 +90,7 @@ export default function Dashboard() {
           </div>
 
           {readingBooks.length > 0 ? (
-            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8">
+            <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 gap-8">
               {readingBooks.map(book => (
                 <BookCard key={book.id} book={book} />
               ))}
