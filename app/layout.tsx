fa25db3019227071f266bb65833c574e269861e0
thibd <thib.del@free.fr>
2025-12-14 17:12:31 +0100

    Fix hydration warning in layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,7 +29,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body
         suppressHydrationWarning
         className={`${inter.variable} ${playfair.variable} antialiased flex min-h-screen bg-background text-text-main`}
